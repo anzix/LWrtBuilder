e7862e0991166855ad4a398eb035066c44871b73
anzix <givefly3@gmail.com>
2026-05-11 01:02:45 +0500

    Автоматизация добавления kmod feed'а

--- sh/specific-setup.sh
+++ sh/specific-setup.sh
@@ -46,16 +46,31 @@ if [[ "$WORKFLOW_NAME" == "AXT-1800" ]]; then
    # git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
    # cat feeds/packages/lang/golang/golang/Makefile
 
+   # Динамически получить хеш Kmod и обновить конфигурацию.
+   #
+   BASE_URL="https://downloads.immortalwrt.org/releases/25.12-SNAPSHOT/targets/qualcommax/ipq60xx/kmods/"
    # Получаем совместимую версию ядра Vermagic из ImmortalWrt
-   REMOTE_IMM_KERNEL_VERSION=$(curl -s "https://downloads.immortalwrt.org/releases/25.12-SNAPSHOT/targets/qualcommax/ipq60xx/kmods/" \
-     | grep -oP "$KERNEL_VERSION" | head -n 1)
+   REMOTE_KERNEL_VERSION=$(curl -s $BASE_URL | grep -oP "$KERNEL_VERSION" | head -n 1)
 
-   # Сравнение
-   if [ "$KERNEL_VERSION" = "$REMOTE_IMM_KERNEL_VERSION" ]; then
+   # Сравнение и получение хеш Kmod
+   if [ "$KERNEL_VERSION" = "$REMOTE_KERNEL_VERSION" ]; then
      echo "Kernel version matches ✅, downloading IMM vermagic MD5 hash file ..."
-     wget -qO- "https://downloads.immortalwrt.org/releases/25.12-SNAPSHOT/targets/qualcommax/ipq60xx/kmods/" | \
-        grep -oP "$KERNEL_VERSION-1-\K[0-9a-f]+"  | head -n 1 > vermagic && \
-        echo "Download successful, current vermagic:" && cat vermagic
+     wget -qO- $BASE_URL | grep -oP "$KERNEL_VERSION-1-\K[0-9a-f]+" | \
+        head -n 1 > vermagic && echo "Download successful, current vermagic:" && cat vermagic
+
+     # Получение имен папок, содержащих хеш-значения (например, 6.6.110-1-xxx...)
+     KMOD_DIR=$(curl -sL $BASE_URL | grep -oE "[0-9.-]+-[a-f0-9]{32}" | tail -n1)
+
+     # Добавление репозитория kmod с фиксированным vermagic
+     if [ -n "$KMOD_DIR" ]; then
+        FULL_KMOD_URL="${BASE_URL}${KMOD_DIR}"
+        echo "✅ Путь к kmods успешно найден: $FULL_KMOD_URL"
+        echo "$FULL_KMOD_URL/packages.adb" >> $GITHUB_WORKSPACE/$FEEDS_CONF
+     else
+       echo "❌ Не удалось получить хеш Kmod. Пожалуйста, проверьте, существует ли эта версия в официальном репозитории: $BASE_URL"
+       exit 1
+     fi
+
      # Saving variables
      VERMAGIC=$(cat vermagic)
      echo "VERMAGIC_FIX=${VERMAGIC}" >> $GITHUB_ENV
